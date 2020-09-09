5a85f9de3e81ff5328d60a62365d1c5111f0dcb6
Sandeep <sunthapa1@hotmail.com>
2020-09-09 19:02:44 +0100

    now able to get prediction from webcam

--- Cube/src/HandSkeleton/handpose.js
+++ Cube/src/HandSkeleton/handpose.js
@@ -4,27 +4,39 @@ async function main2(){
     const model = await handpose.load();
     console.log('The model has been loaded');
 
-    rawImage = document.getElementById('canvasimg');
-    canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-    canvas.height = rawImage.height;
-    canvas.width = rawImage.width;
-    ctx.drawImage(rawImage,30,30);
-    const data = ctx.getImageData(60,60,200,100);
-    //tf.browser.fromPixels(image)
-    const predictions = await model.estimateHands(canvas);
-    console.log(predictions)
-    if(predictions.length>0){
-        for (let i = 0; i < predictions.length; i++) {
-            const keypoints = predictions[i].landmarks;
- 
-            // Log hand keypoints.
-            for (let i = 0; i < keypoints.length; i++) {
-                const [x, y, z] = keypoints[i];
-                console.log(`Keypoint ${i}: [${x}, ${y}, ${z}]`);
+    console.log('Loading Webcam')
+    const webcam = new Webcam(document.getElementById('wc'));
+    await webcam.setup();
+    console.log('Webcame is loaded');
+    while(true){
+        //const predictions = tf.tidy(async ()=>{
+            //const img = webcam.capture();
+            //const predictions =  await model.estimateHands(img);
+            //console.log(predictions)
+            //return predictions;
+        //});
+
+        
+        const img = webcam.capture();
+
+        const predictions =  await model.estimateHands(img);
+
+        if(predictions.length>0){
+            for (let i = 0; i < predictions.length; i++) {
+                const keypoints = predictions[i].landmarks;
+    
+                // Log hand keypoints.
+                for (let i = 0; i < keypoints.length; i++) {
+                    const [x, y, z] = keypoints[i];
+                    console.log(`Keypoint ${i}: [${x}, ${y}, ${z}]`);
+                }
             }
         }
+        img.dispose();
+        await tf.nextFrame();
     }
     
+    
 }
+
 main2();
